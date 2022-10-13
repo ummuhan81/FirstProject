b8b72242650f74ddb0bec647c2bc0d06c7105a49
ummuhancebeci <ummuhanalgl81@gmail.com>
2022-10-13 08:48:03 +0200

    kodlara calisiliyor.

---
+++ FirstProject/day02.java
@@ -0,0 +1,30 @@
+package FirstProject;
+
+public class day02 {
+    public static void main(String[] args) {
+        /* Sinif isimleri olusturulurken tek kelimeden olusuyorsa sinif isimimizin bas harfi buyuk eger birden
+         fazla sinif isminden olusuyorsa her kelimenin basharfi buyuk olacak sekilde yazilir. buna pascal notasyonu
+         denilir.
+
+         */
+        // Degisken tanimlama
+        int kenar = 5 ;
+        System.out.println(kenar*kenar );
+
+        /*programlamada asil isi operatorler yapar. esittirin buradaki gorevi esaymint yani atama yapmaktir.
+        esittirin sagindaki 5 sayisi bir literaldir.kenar ise bir degiskendir.int ise degiskenin tipidir.
+        bir kisinin kilosunu tutmak istedigimde ise kesirli bir sayi kullanacagimiy icin degiskenimiz farkli olacak.
+        aciklama satiri tek satir yazacaksam // koyuyoruz birden fazla satir yazacaksak /*koyuyoruz.
+
+    Degisken Tanimlama kurallari:
+    1. java anahtar kelimeleri kullanilamaz.( package , pablic gibi )
+    2. Degisken isminde bosluk bulunamaz.
+    3. degiskenin ismi sayi ile baslayamaz .
+    4. degisken isminin sonunda veya ortasinda sayi bulunabilir.
+
+         */
+
+
+
+    }
+}
