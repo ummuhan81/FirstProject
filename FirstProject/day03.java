9279466e279deafefa0a236aeb322995d4aaadcb
ummuhancebeci <ummuhanalgl81@gmail.com>
2022-10-13 08:48:22 +0200

    kodlara calisiliyor.

---
+++ FirstProject/day03.java
@@ -0,0 +1,19 @@
+package FirstProject;
+
+public class day03 {
+    public static void main(String[] args) {
+        /* Kisa kenar ve uzun kenar uzunluklari verilen bir dikdortgenin alanini hesaplayan java programi yaziniz.
+        ( int tam sayi veri tipini kullaniyorum.
+        int kisaKenar=4;
+        int uzunKenar=6;
+        BU SEKILDE YAZABILECEGIM GIBI SU SEKILDEDE YAZABILIRIM.
+        int kisaKenar=4, uzunKenar= 6;
+         */
+        int kisaKenar=6;
+        int uzunKenar=9 ;
+        int alan = kisaKenar*uzunKenar;
+        System.out.println(alan);
+
+    }
+
+}
